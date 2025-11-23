be48fec9186b358b281033d3510cd1f8ff1043c4
Luis Frota <luisjuniorfrota@gmail.com>
2025-11-23 20:58:43 -0300

    edit

--- database/main.py
+++ database/main.py
@@ -5,15 +5,26 @@ db.connect()
 
 db.create_tables([BaseModel,Client, BoardGame, Rent, RentBoardGame])
 
-def createClient(newClient):
-    
+def createClient(newClient):  
     Client.create(name = newClient.name, email = newClient.email)
     pass
 
-obj = ClientData(name="example", email="example@gmail.com")
-createClient(obj)
+def editClient(id, newClient):
+    oldClient = Client.get_by_id(id)
+    oldClient.name = newClient.name
+    oldClient.email = newClient.email
+    oldClient.save()
+    pass
+
+def getClient(id):
+    pass
+
+def deleteClient(id):
+    pass
+
+obj = ClientData(name="editName", email="editEmail@gmail.com")
+editClient(1,obj)
 
 listClient = Client.select()
-
-for c in listClient:
+for c in listClient: 
     print(c.name)
